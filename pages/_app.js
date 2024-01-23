87aeb27b285fb2bdf0dbcb704c23d8b45731488d
Brendan Francis <B.Francis@SprengerV.com>
2024-01-23 18:35:47 -0500

    Added Navbar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
+import Navbar from '../components/Navbar.js'
 
 function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
       <meta name="viewport" content="width=device-width" initial-scale="1" />
     </Head>
+    <Navbar />
     <Component {...pageProps} />
   </>)
 }
