e009003c1fb1ea3cb1d026ce2190c203a5458568
Brendan Francis <B.Francis@SprengerV.com>
2024-01-27 23:15:59 -0500

    Home page image layout roughed out.

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,21 @@ export default function Home() {
     </Head>
     <div className="container-fluid">
       <div className="row">
-        <Image src="/#" className="img-fluid col-md-5" />
+        <div className="col-12 col-md-5 ms-auto me-auto me-md-5">
+          <Image src="/#" className="img-fluid float-end" width="400" height="300"/>
+        </div>
       </div>
       <div className="row">
-        <h1>
-          SprengerV.com
-        </h1>
+        <div className="col-12 mx-auto">
+          <h1 className="text-center">
+            SprengerV.com
+          </h1>
+        </div>
       </div>
       <div className="row">
-        <Image src="/#" className="img-fluid col-md-5" />
+        <div className="col col-md-5 ms-auto ms-md-5 me-auto justify-content-end">
+          <Image src="/#" className="img-fluid" width="400" height="300"/>
+        </div>  
       </div>
     </div>
   </>)
